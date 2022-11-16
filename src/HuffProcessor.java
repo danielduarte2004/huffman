096cd0e90c73f2c33e204dbbc9f0ef1a95a2351f
Daniel Duarte <daniel.duarte@duke.edu>
2022-11-15 20:33:04 -0500

    project finished (hopefully)

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -63,14 +63,20 @@ public class HuffProcessor {
 	 *            Buffered bit stream writing to the output file.
 	 */
 	public void compress(BitInputStream in, BitOutputStream out){
+    int[] counts = getCounts(in);
+    HuffNode root = makeTree(counts);
+    in.reset();
+    out.writeBits(BITS_PER_INT, HUFF_TREE);
+    writeTree(root, out);
+    String[] encodings = new String[ALPH_SIZE+1];
+    makeEncodings(root, "", encodings);
 
+    for (String code : encodings)
+      out.writeBits(code.length(), Integer.parseInt(code,2));
+     
+    String eof = encodings[PSEUDO_EOF];
+    out.writeBits(eof.length(), Integer.parseInt(eof,2));
 
-		// remove all this code when implementing compress
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
-		}
 		out.close();
 	}
 
@@ -84,14 +90,106 @@ public class HuffProcessor {
 	 *            Buffered bit stream writing to the output file.
 	 */
 	public void decompress(BitInputStream in, BitOutputStream out){
+    int bits = in.readBits(BITS_PER_INT);
+    if (bits != HUFF_TREE)
+      throw new HuffException("invalid magic number " + bits);
+    
+    HuffNode root = readTree(in);
+    HuffNode current = root;
 
-		// remove all code when implementing decompress
+    while (true) {
+      bits = in.readBits(1);
+      if (bits == -1)
+        throw new HuffException("bad input, no PSEUDO_EOF");
+      
+      current = bits == 0 ? current.left : current.right;
 
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
-		}
+      if (isLeaf(current)) {
+        if (current.value == PSEUDO_EOF)
+          break;
+        
+        out.writeBits(BITS_PER_WORD, current.value);
+        current = root;
+      }
+    }
 		out.close();
 	}
+
+  private HuffNode readTree(BitInputStream in) {
+    int bit = in.readBits(1);
+
+    if (bit == -1) 
+      throw new HuffException("Reading bit failed.");
+
+    if (bit == 0) {
+      HuffNode left = readTree(in);
+      HuffNode right = readTree(in);
+
+      return new HuffNode(0, 0, left, right);
+    }
+
+    int letter = in.readBits(BITS_PER_WORD + 1);
+    return new HuffNode(letter, 0, null, null);
+  }
+
+  private boolean isLeaf(HuffNode h) {
+    return h.left == null && h.right == null;
+  }
+
+  private int[] getCounts(BitInputStream in) {
+    int[] counts = new int[ALPH_SIZE];
+
+    while (true){
+			int val = in.readBits(BITS_PER_WORD);
+			if (val == -1) break;
+      counts[val]++;
+		}
+
+    return counts;
+  }
+
+  private HuffNode makeTree(int[] counts) {
+    PriorityQueue<HuffNode> pq = new PriorityQueue<>();
+    for (int i = 0; i < counts.length; i++) {
+      if (counts[i] == 0)
+        break;
+      
+      pq.add(new HuffNode(i, counts[i], null, null));
+    }
+
+    pq.add(new HuffNode(PSEUDO_EOF, 1, null, null));
+
+    while (pq.size() > 1) {
+      HuffNode left = pq.remove();
+      HuffNode right = pq.remove();
+      HuffNode t = new HuffNode(0, left.weight + right.weight, left, right);
+      pq.add(t);
+    }
+    HuffNode root = pq.remove();
+
+    return root;
+  } 
+
+  private void writeTree(HuffNode tree, BitOutputStream out) {
+    if (isLeaf(tree)) {
+      out.writeBits(1, 1);
+      out.writeBits(BITS_PER_WORD+1, tree.value);
+      return;
+    }
+
+    out.writeBits(1, 0);
+
+    writeTree(tree.left, out);
+    writeTree(tree.right, out);
+  }
+
+  private void makeEncodings(HuffNode root, String path, String[] encodings) {
+    if (isLeaf(root)) {
+      encodings[root.value] = path;
+      return;
+    }
+
+    makeEncodings(root.left,  path + "0", encodings);
+    makeEncodings(root.right, path + "1", encodings);
+  } 
 }
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    int[] res = new int[10];
+
+    res[3]++;
+    System.out.println(res[3]);
+  }
+}
